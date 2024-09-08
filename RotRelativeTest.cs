66429280dbe6664141eb62b4d320ea96b284b700
gadget114514 <34733747+gadget114514@users.noreply.github.com>
2024-09-08 13:00:05 +0900

    Add files via upload

---
+++ RotRelativeTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotTest : MonoBehaviour
+{
+	public GameObject a;
+	public GameObject b;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+	    Quaternion r = a.transform.rotation;
+	    Quaternion q = b.transform.rotation;
+	    
+	    Quaternion k = q * Quaternion.Inverse(r);
+	    
+	    Vector3 dir = k.eulerAngles;
+	    
+	    Debug.Log("angle=" + dir.x + " " + dir.y + " " + dir.z);
+    
+	    Vector3 d = k * Vector3.forward;
+	    
+	    Debug.Log("vec=" + d.x + " " + d.y + " " + d.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
